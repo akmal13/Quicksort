96ae233d9c5c48a04e9bc15c7dd8f2d578ee5e15
akmal13 <akmalnajid@gmail.com>
2015-12-05 15:06:28 +0700

    Bagian Recursive Quicksort
    dari http://www.algolist.net/Algorithms/Sorting/Quicksort

---
+++ quicksort.java
@@ -0,0 +1,7 @@
+void quickSort(int arr[], int left, int right) {
+      int index = partition(arr, left, right);
+      if (left < index - 1)
+            quickSort(arr, left, index - 1);
+      if (index < right)
+            quickSort(arr, index, right);
+}
